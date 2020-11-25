d147924be6e3ce1fac3bb5c1f97e2174fc2e2234
Chan, York T <york.t.chan@accenture.com>
2020-11-25 22:17:01 +0800

    add toolbar to MainActivity

--- app/src/main/java/com/example/minimoneybox/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/minimoneybox/ui/main/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity: BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
 }
