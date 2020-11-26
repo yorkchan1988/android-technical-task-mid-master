cefc840ebce76d895e6f13811b023c44bb8936a5
Chan, York T <york.t.chan@accenture.com>
2020-11-27 00:18:25 +0800

    add OneOffPaymentsResponse

---
+++ app/src/main/java/com/example/minimoneybox/models/response/OneOffPaymentsResponse.kt
@@ -0,0 +1,10 @@
+package com.example.minimoneybox.models.response
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class OneOffPaymentsResponse(
+    @SerializedName("Moneybox")
+    @Expose
+    var moneybox : String
+)
