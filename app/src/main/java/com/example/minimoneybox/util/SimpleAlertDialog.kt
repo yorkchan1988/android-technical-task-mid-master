951df33af6078619046550f009079204200533ab
Chan, York T <york.t.chan@accenture.com>
2020-11-29 02:19:32 +0800

    change class to object for SimpleAlertDialog

--- app/src/main/java/com/example/minimoneybox/util/SimpleAlertDialog.kt
+++ app/src/main/java/com/example/minimoneybox/util/SimpleAlertDialog.kt
@@ -4,19 +4,16 @@ import android.app.Activity
 import android.content.DialogInterface
 import androidx.appcompat.app.AlertDialog
 
-class SimpleAlertDialog {
+object SimpleAlertDialog {
+    fun showAlert(activity: Activity?, title: String, message: String) {
+        activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle(title)
+            builder.setMessage(message)
+            builder.setPositiveButton(android.R.string.ok) { dialog: DialogInterface?, which: Int ->
 
-    companion object {
-        fun showAlert(activity: Activity?, title: String, message: String) {
-            activity?.let {
-                val builder = AlertDialog.Builder(it)
-                builder.setTitle(title)
-                builder.setMessage(message)
-                builder.setPositiveButton(android.R.string.ok) { dialog: DialogInterface?, which: Int ->
-
-                }
-                builder.show()
             }
+            builder.show()
         }
     }
 }
