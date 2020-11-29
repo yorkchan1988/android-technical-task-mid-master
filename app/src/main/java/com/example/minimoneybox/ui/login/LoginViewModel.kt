a801dd9806048aaa6526bff7545c700659d04e1d
Chan, York T <york.t.chan@accenture.com>
2020-11-29 17:28:35 +0800

    finish LoginViewModelUnitTest

--- app/src/main/java/com/example/minimoneybox/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/minimoneybox/ui/login/LoginViewModel.kt
@@ -2,9 +2,13 @@ package com.example.minimoneybox.ui.login
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.minimoneybox.exception.ApiException
 import com.example.minimoneybox.models.LoginSession
+import com.example.minimoneybox.models.response.ValidationError
 import com.example.minimoneybox.network.ApiResource
 import com.example.minimoneybox.repository.LoginRepository
+import com.example.minimoneybox.util.Constants.Companion.LOGIN_VALIDATION_ERROR_KEY_EMAIL
+import com.example.minimoneybox.util.Constants.Companion.LOGIN_VALIDATION_ERROR_KEY_PASSWORD
 import io.reactivex.rxkotlin.subscribeBy
 import javax.inject.Inject
 
@@ -15,7 +19,11 @@ class LoginViewModel @Inject constructor(loginRepository: LoginRepository) : Vie
     }
 
     // LiveData
-    var loginApiStatus : MutableLiveData<ApiResource<LoginSession>> = MutableLiveData()
+    var apiStatus : MutableLiveData<ApiResource<LoginSession>> = MutableLiveData()
+    var error: MutableLiveData<Exception> = MutableLiveData()
+
+    var emailErrorText: MutableLiveData<String> = MutableLiveData("")
+    var passwordErrorText: MutableLiveData<String> = MutableLiveData("")
 
     // API
     private val loginRepository: LoginRepository = loginRepository
@@ -23,9 +31,57 @@ class LoginViewModel @Inject constructor(loginRepository: LoginRepository) : Vie
     fun login(email: String, password: String) {
         // if fail with validation errors
         // update livedata of error messages of edit text
-        loginRepository.login(email, password).subscribeBy {
-            loginApiStatus.postValue(it)
-//            TODO("if fail with validation errors, update livedata of error messages of edit text")
+        loginRepository.login(email, password).subscribeBy(
+            onNext = {
+                when (it.status) {
+                    ApiResource.ApiStatus.SUCCESS -> {
+                        // if success
+                        // return apiStatus directly
+                        apiStatus.postValue(it)
+                    }
+                    ApiResource.ApiStatus.ERROR -> {
+                        // if error
+                        // return apiStatus
+                        apiStatus.postValue(it)
+                        it.error?.let {errorRes ->
+                            errorRes.validationErrors?.let {list ->
+                                // check if validationErrors.size == 0
+                                if (list.size == 0) {
+                                    // if yes, return error with ApiException
+                                    errorRes.message?.let {msg ->
+                                        val name = errorRes.errorName ?: "Error"
+                                        error.postValue(ApiException(name, msg))
+                                    }
+                                }
+                                // if no, update errorErrorText and passwordErrorText LiveData
+                                else {
+                                    val errors = list.toSet()
+                                    val emailErrorMsg = getValidationErrorMessage(errors, LOGIN_VALIDATION_ERROR_KEY_EMAIL)
+                                    val passwordErrorMsg = getValidationErrorMessage(errors, LOGIN_VALIDATION_ERROR_KEY_PASSWORD)
+
+                                    emailErrorText.postValue(emailErrorMsg)
+                                    passwordErrorText.postValue(passwordErrorMsg)
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            onError = {
+                apiStatus.postValue(ApiResource.Error(null, null))
+                error.postValue(it as Exception)
+            }
+        )
+    }
+
+    fun getValidationErrorMessage(errors: Set<ValidationError>, key: String): String {
+        var errorMessage = ""
+        val errorMsg = errors.find {
+            it.errorName == key
         }
+        errorMsg?.let {
+            errorMessage = it.message ?: ""
+        }
+        return errorMessage
     }
 }
---
+++ app/src/test/java/com/example/minimoneybox/ui/LoginViewModelUnitTest.kt
@@ -0,0 +1,178 @@
+package com.example.minimoneybox.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.minimoneybox.exception.ApiException
+import com.example.minimoneybox.models.LoginSession
+import com.example.minimoneybox.models.Session
+import com.example.minimoneybox.models.response.ErrorResponse
+import com.example.minimoneybox.models.response.ValidationError
+import com.example.minimoneybox.network.ApiResource
+import com.example.minimoneybox.repository.LoginRepository
+import com.example.minimoneybox.ui.login.LoginViewModel
+import com.example.minimoneybox.util.Constants
+import com.example.minimoneybox.util.Constants.Companion.LOGIN_EMAIL
+import com.example.minimoneybox.util.Constants.Companion.LOGIN_PASSWORD
+import com.example.minimoneybox.util.FileUtils
+import io.reactivex.Observable
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import retrofit2.HttpException
+import retrofit2.Response
+import java.lang.Exception
+import java.net.HttpURLConnection
+import kotlin.test.assertEquals
+
+class LoginViewModelUnitTest {
+
+    @get:Rule
+    var instantExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var loginRepository: LoginRepository
+
+    lateinit var loginViewModel: LoginViewModel
+
+    @Before
+    fun setUp() {
+        // create mock objects
+        MockitoAnnotations.initMocks(this)
+        loginViewModel = LoginViewModel(loginRepository)
+    }
+
+    @Test
+    fun getValidationErrorMessage_getEmailErrorMessage() {
+        // GIVEN
+        val errors = listOf(
+            ValidationError("Email", "The email address you entered is not valid."),
+            ValidationError("Password", "Your password must be at least 10 characters and include one number, one upper case character and one lower case character.")
+        )
+        // WHEN
+        val errorMessage = loginViewModel.getValidationErrorMessage(errors.toSet(),
+            Constants.LOGIN_VALIDATION_ERROR_KEY_EMAIL
+        )
+        // THEN
+        assertEquals("The email address you entered is not valid.", errorMessage)
+    }
+
+    @Test
+    fun getValidationErrorMessage_getPasswordErrorMessage() {
+        // GIVEN
+        val errors = listOf(
+            ValidationError("Email", "The email address you entered is not valid."),
+            ValidationError("Password", "Your password must be at least 10 characters and include one number, one upper case character and one lower case character.")
+        )
+        // WHEN
+        val errorMessage = loginViewModel.getValidationErrorMessage(errors.toSet(),
+            Constants.LOGIN_VALIDATION_ERROR_KEY_PASSWORD
+        )
+        // THEN
+        assertEquals("Your password must be at least 10 characters and include one number, one upper case character and one lower case character.", errorMessage)
+    }
+
+    @Test
+    fun getValidationErrorMessage_NoSuchKey() {
+        // GIVEN
+        val errors = listOf<ValidationError>()
+        // WHEN
+        val errorMessage = loginViewModel.getValidationErrorMessage(errors.toSet(),
+            Constants.LOGIN_VALIDATION_ERROR_KEY_EMAIL
+        )
+        // THEN
+        assertEquals("", errorMessage)
+    }
+
+    @Test
+    fun login_apiStatusSuccess() {
+        // GIVEN
+        val email = LOGIN_EMAIL
+        val password = LOGIN_PASSWORD
+        val token = "gnOKw1sLpvLeot8IHesQydAyfWtBW5+z4SCCB46UyTY="
+        val responseData = LoginSession(Session(token))
+        val apiResource = ApiResource.Success(responseData)
+        Mockito.`when`(loginRepository.login(email, password))
+            .thenReturn(Observable.just(apiResource))
+        // WHEN
+        loginViewModel.login(email, password)
+        // THEN
+        assertEquals(apiResource, loginViewModel.apiStatus.value)
+        assertEquals(null, loginViewModel.error.value)
+        assertEquals("", loginViewModel.emailErrorText.value)
+        assertEquals("", loginViewModel.passwordErrorText.value)
+    }
+
+    @Test
+    fun login_apiStatusErrorWithoutValidationErrors() {
+        // GIVEN
+        val email = "abc@abc.com"
+        val password = LOGIN_PASSWORD
+        // create httpException
+        val responseBody = FileUtils.readTestResourceFile("login_api_invalid_email.json", "login")
+        val response = Response.error<HttpException>(HttpURLConnection.HTTP_BAD_REQUEST,responseBody.toResponseBody("text/plain".toMediaTypeOrNull()))
+        val httpException = HttpException(response)
+        // create ApiResource
+        val errorResponse = ErrorResponse.fromHttpException(httpException)
+        val apiResource = ApiResource.Error<LoginSession>(null, errorResponse)
+        // mock response
+        Mockito.`when`(loginRepository.login(email, password))
+            .thenReturn(Observable.just(apiResource))
+        // WHEN
+        loginViewModel.login(email, password)
+        // THEN
+        assertEquals(apiResource, loginViewModel.apiStatus.value)
+        val expectedApiException = ApiException("Login failed","Incorrect email address or password. Please check and try again.")
+        val actualApiException = loginViewModel.error.value as ApiException
+        assertEquals(expectedApiException.name, actualApiException.name)
+        assertEquals(expectedApiException.message, actualApiException.message)
+        assertEquals("", loginViewModel.emailErrorText.value)
+        assertEquals("", loginViewModel.passwordErrorText.value)
+    }
+
+    @Test
+    fun login_apiStatusErrorWithValidationErrors() {
+        // GIVEN
+        val email = ""
+        val password = ""
+        // create httpException
+        val responseBody = FileUtils.readTestResourceFile("login_api_empty_email_and_empty_password.json", "login")
+        val response = Response.error<HttpException>(HttpURLConnection.HTTP_BAD_REQUEST,responseBody.toResponseBody("text/plain".toMediaTypeOrNull()))
+        val httpException = HttpException(response)
+        // create ApiResource
+        val errorResponse = ErrorResponse.fromHttpException(httpException)
+        val apiResource = ApiResource.Error<LoginSession>(null, errorResponse)
+        // mock response
+        Mockito.`when`(loginRepository.login(email, password))
+            .thenReturn(Observable.just(apiResource))
+        // WHEN
+        loginViewModel.login(email, password)
+        // THEN
+        assertEquals(apiResource, loginViewModel.apiStatus.value)
+        assertEquals(null, loginViewModel.error.value)
+        assertEquals("The email address you entered is not valid.", loginViewModel.emailErrorText.value)
+        assertEquals("Your password must be at least 10 characters and include one number, one upper case character and one lower case character.", loginViewModel.passwordErrorText.value)
+    }
+
+    @Test
+    fun login_unexpectedError() {
+        // GIVEN
+        val email = ""
+        val password = ""
+        // mock response
+        Mockito.`when`(loginRepository.login(email, password))
+            .thenReturn(Observable.error(Exception("Unexpected Error")))
+        // WHEN
+        loginViewModel.login(email, password)
+        // THEN
+        assertEquals(null, loginViewModel.apiStatus.value?.data)
+        assertEquals(null, loginViewModel.apiStatus.value?.error)
+        assertEquals("Unexpected Error", loginViewModel.error.value?.message)
+        assertEquals("", loginViewModel.emailErrorText.value)
+        assertEquals("", loginViewModel.passwordErrorText.value)
+    }
+
+}
